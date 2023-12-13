9b0cadc6ddf07e01f6b38be012eb4037fdf7ed35
Kshantru <64325322+Kshantru@users.noreply.github.com>
2023-12-12 19:09:06 -0500

    Reduced buffer size and iteration for DT
    Buffer size reduced to 32mb

--- test/double_tree_test.cpp
+++ test/double_tree_test.cpp
@@ -181,15 +181,15 @@ int main(int argc, char** argv) {
 
     // Buffer sizes used for tests.
     std::vector<size_t> buffer_sizes = {
-        0, 32, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 8388608, 67108864, 536870912
+        0, 32, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 32768000
     };
 
-    // Number of iterations to run for each buffer size.
     std::vector<size_t> iterations = {
-        100000, 100000, 100000, 100000,
-        1000, 1000, 1000, 1000,
+        10000, 10000, 10000, 10000,
+        500, 500, 500, 500,
         100, 50, 10, 1
     };
+
     // Test on either CPU and GPU.
     if(input == "cpu") {
         TestCollectivesCPU(buffer_sizes, iterations);
